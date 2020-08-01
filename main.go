7413c1cd914554d5e5d05c00b8cd8143576689d0
anoriqq <shota.yoshikawa@anoriqq.com>
2020-08-01 21:51:59 +0900

    chore: write to browser

--- main.go
+++ main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"io"
-	"net"
 	"net/http"
-	"os"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "http.ResponseWriter sample")
+}
+
 func main() {
-	conn, err := net.Dial("tcp", "ascii.jp:80")
-	if err != nil {
-		panic(err)
-	}
-	req, err := http.NewRequest("GET", "http://ascii.jp", nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Write(conn)
-	// プロセスが落ちないのはなぜだろう
-	io.Copy(os.Stdout, conn)
-	req.Body.Close()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
