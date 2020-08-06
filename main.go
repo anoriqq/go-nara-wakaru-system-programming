85a52d37e7641a0e4a99f6ae73dcf2de1151794f
anoriqq <shota.yoshikawa@anoriqq.com>
2020-08-06 21:48:50 +0900

    read network

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("main.go")
+	conn, err := net.Dial("tcp", "ascii.jp:80")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-	io.Copy(os.Stdout, file)
+	conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
+	res, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	// ヘッダーを表示してみる
+	fmt.Println(res.Header)
+	// ボディーを表示してみる。最後にはClose()すること
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
 }
