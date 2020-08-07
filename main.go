697f4dd50954017d93fe78fa99b6b41a4e4a9a88
anoriqq <shota.yoshikawa@anoriqq.com>
2020-08-07 16:07:54 +0900

    use bufio.Scanner to separate strings with a new line

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"strings"
 )
 
@@ -12,12 +11,8 @@ var source = `1行目
 3行目`
 
 func main() {
-	reader := bufio.NewReader(strings.NewReader(source))
-	for {
-		line, err := reader.ReadString('\n')
-		fmt.Printf("%#v\n", line)
-		if err == io.EOF {
-			break
-		}
+	scanner := bufio.NewScanner(strings.NewReader(source))
+	for scanner.Scan() {
+		fmt.Printf("%#v\n", scanner.Text())
 	}
 }
