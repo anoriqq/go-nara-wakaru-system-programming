2f9dc0df9603d943165dea4f678f4a72d3efe04d
anoriqq <shota.yoshikawa@anoriqq.com>
2020-08-08 13:32:21 +0900

    use flag

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("old.txt")
+	sourceFileName := flag.String("s", "old.txt", "source file name")
+	outputFileName := flag.String("o", "new.txt", "output file name")
+	flag.Parse()
+
+	file, err := os.Open(*sourceFileName)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	newFile, err := os.Create("new.txt")
+	newFile, err := os.Create(*outputFileName)
 	if err != nil {
 		panic(err)
 	}
