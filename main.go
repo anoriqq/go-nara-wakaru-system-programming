d8e7dd93ac1fbaa5e8f0026cbdb9afa42531d59e
anoriqq <shota.yoshikawa@anoriqq.com>
2020-08-09 17:39:58 +0900

    create binary file

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"flag"
+	"crypto/rand"
 	"io"
 	"os"
 )
 
 func main() {
-	sourceFileName := flag.String("s", "old.txt", "source file name")
-	outputFileName := flag.String("o", "new.txt", "output file name")
-	flag.Parse()
-
-	file, err := os.Open(*sourceFileName)
+	file, err := os.Create("random.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	newFile, err := os.Create(*outputFileName)
-	if err != nil {
-		panic(err)
-	}
-	defer newFile.Close()
-	io.Copy(newFile, file)
+	io.CopyN(file, rand.Reader, 1024)
 }
